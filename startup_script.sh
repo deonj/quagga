1ce586fddcef95033f303aaadb1861ded4917eff
Deon Joseph <deonj@users.noreply.github.com>
2020-08-16 16:02:33 -0300

    Update startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # "Starting the zebra daemon"
-command="/usr/sbin/zebra -d"
+/usr/sbin/zebra -d
 # "Starting the ospf daemon"
-command="/usr/sbin/ospfd -d"
+/usr/sbin/ospfd -d
 # "Starting the bgp daemon"
-command="/usr/sbin/bgpd -d"
+/usr/sbin/bgpd -d
 
 # Delete default route
-ip route del default
+#command='ip route del default'
